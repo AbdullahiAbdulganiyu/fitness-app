547cbcb5d27142c03b69fb023d7f6d40556bc679
AbdullahiAbdulganiyu <ludba020@gmail.com>
2025-04-27 14:01:27 +0100

    importing the Hero component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Generator from "./components/Generator";
 import Workout from "./components/Workout";
+import Hero from "./components/Hero";
 
 function App() {
   return (
