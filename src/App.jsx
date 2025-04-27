8c24594094ab74adf01337575cdb112efccc5148
AbdullahiAbdulganiyu <ludba020@gmail.com>
2025-04-27 14:12:58 +0100

    adding some classes to nicely style the contents

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Hero from "./components/Hero";
 
 function App() {
   return (
-    <main>
+    <main className="min-h-screen flex flex-col bg-gradient-to-r from-slate-800 to-slate-950 text-white text-sm sm:text-base">
       <Hero />
       <Generator />
       <Workout />
