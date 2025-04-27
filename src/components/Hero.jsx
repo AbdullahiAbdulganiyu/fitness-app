e519998ff6471875eaf05bde4c22cb02c2b612ac
AbdullahiAbdulganiyu <ludba020@gmail.com>
2025-04-27 14:20:20 +0100

    adding some content to the Hero component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,18 @@
 import React from "react";
 
 export default function Hero() {
-  return <div>Hero</div>;
+  return (
+    <div>
+      <p>IT'S TIME TO GET</p>
+      <h1>
+        Swole<span>normous</span>
+      </h1>
+      <p>
+        I hereby acknowledge that I may become
+        <span>unbelievably swolenormous</span> and accept all the risk of
+        becoming the local <span>mass montrosity</span> afflicted with sevre
+        body dimorphia, unable to fit through doors.
+      </p>
+    </div>
+  );
 }
