f45936bde07f1b8618ba7f64bda171bf78f8b6d4
AbdullahiAbdulganiyu <ludba020@gmail.com>
2025-04-27 14:42:50 +0100

    wraping the paragraph and h1 in a div

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,10 +3,12 @@ import React from "react";
 export default function Hero() {
   return (
     <div className="min-h-screen flex flex-col gap-10 items-center justify-center text-center">
-      <p>IT'S TIME TO GET</p>
-      <h1 className="uppercase font-semibold text-4xl sm:text-5xl md:text-6xl lg:text-7xl">
-        Swole<span className="text-blue-400">normous</span>
-      </h1>
+      <div>
+        <p>IT'S TIME TO GET</p>
+        <h1 className="uppercase font-semibold text-4xl sm:text-5xl md:text-6xl lg:text-7xl">
+          Swole<span className="text-blue-400">normous</span>
+        </h1>
+      </div>
       <p className="text-sm md:text-base font-light">
         I hereby acknowledge that I may become
         <span className="text-blue-400 font-medium">
