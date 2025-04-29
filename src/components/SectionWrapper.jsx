66f401e70619ed3b3c4c2ec2bcace2c1d4672993
AbdullahiAbdulganiyu <ludba020@gmail.com>
2025-04-29 14:27:11 +0100

    adding the header

--- src/components/SectionWrapper.jsx
+++ src/components/SectionWrapper.jsx
@@ -3,7 +3,9 @@ import React from "react";
 export default function SectionWrapper({ children, header, title }) {
   return (
     <section className="min-h-screen flex flex-col gap-10">
-      <div className="bg-slate-950 py-10 flex flex-col gap-4 justify-center items-center"></div>
+      <div className="bg-slate-950 py-10 flex flex-col gap-4 justify-center items-center">
+        <p>{header}</p>
+      </div>
     </section>
   );
 }
