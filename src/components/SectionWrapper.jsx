cba2ff8252ddf8f2b77361ce7678f7068430fe18
AbdullahiAbdulganiyu <ludba020@gmail.com>
2025-04-29 14:32:33 +0100

    adding some classes to style the paragraph

--- src/components/SectionWrapper.jsx
+++ src/components/SectionWrapper.jsx
@@ -4,7 +4,7 @@ export default function SectionWrapper({ children, header, title }) {
   return (
     <section className="min-h-screen flex flex-col gap-10">
       <div className="bg-slate-950 py-10 flex flex-col gap-2 justify-center items-center">
-        <p>{header}</p>
+        <p className="uppercase font-medium">{header}</p>
         <h2>
           {title[0]}
           <span>{title[1]}</span>
