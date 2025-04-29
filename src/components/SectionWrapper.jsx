f421d0bfc3a1d8f665605994e18234d41a357c04
AbdullahiAbdulganiyu <ludba020@gmail.com>
2025-04-29 14:19:26 +0100

    recieving the headder and title prop

--- src/components/SectionWrapper.jsx
+++ src/components/SectionWrapper.jsx
@@ -1,5 +1,5 @@
 import React from "react";
 
-export default function SectionWrapper({ children }) {
+export default function SectionWrapper({ children, header, title }) {
   return <div>{children}</div>;
 }
