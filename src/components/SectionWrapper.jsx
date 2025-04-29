c7e08b6ef07454d0b1e5e513372593557b9cef6d
AbdullahiAbdulganiyu <ludba020@gmail.com>
2025-04-29 14:29:03 +0100

    adding the title

--- src/components/SectionWrapper.jsx
+++ src/components/SectionWrapper.jsx
@@ -5,6 +5,11 @@ export default function SectionWrapper({ children, header, title }) {
     <section className="min-h-screen flex flex-col gap-10">
       <div className="bg-slate-950 py-10 flex flex-col gap-4 justify-center items-center">
         <p>{header}</p>
+        <h2>
+          {title[0]}
+          <span>{title[1]}</span>
+          {title[2]}
+        </h2>
       </div>
     </section>
   );
