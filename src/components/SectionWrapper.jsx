7aa70e199a9dfc39ca1f60072b6e02db11b4f5a4
AbdullahiAbdulganiyu <ludba020@gmail.com>
2025-04-29 14:25:57 +0100

    adding a div and some styles to it

--- src/components/SectionWrapper.jsx
+++ src/components/SectionWrapper.jsx
@@ -2,6 +2,8 @@ import React from "react";
 
 export default function SectionWrapper({ children, header, title }) {
   return (
-    <section className="min-h-screen flex flex-col gap-10">{children}</section>
+    <section className="min-h-screen flex flex-col gap-10">
+      <div className="bg-slate-950 py-10 flex flex-col gap-4 justify-center items-center"></div>
+    </section>
   );
 }
