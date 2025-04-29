bc9d3129fc57e3ba26ef4afd1dbd6a29f2f17542
AbdullahiAbdulganiyu <ludba020@gmail.com>
2025-04-29 14:23:47 +0100

    adding some classes to style the section

--- src/components/SectionWrapper.jsx
+++ src/components/SectionWrapper.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 
 export default function SectionWrapper({ children, header, title }) {
-  return <section>{children}</section>;
+  return (
+    <section className="min-h-screen flex flex-col gap-10">{children}</section>
+  );
 }
