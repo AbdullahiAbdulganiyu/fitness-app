ef9e8f4349ab4216f07c013cdf1398257c7482e1
AbdullahiAbdulganiyu <ludba020@gmail.com>
2025-04-29 14:35:12 +0100

    styling the second element of the title

--- src/components/SectionWrapper.jsx
+++ src/components/SectionWrapper.jsx
@@ -7,7 +7,7 @@ export default function SectionWrapper({ children, header, title }) {
         <p className="uppercase font-medium">{header}</p>
         <h2>
           {title[0]}
-          <span>{title[1]}</span>
+          <span className="uppercase text-blue-400">{title[1]}</span>
           {title[2]}
         </h2>
       </div>
