315f8f6ec81d549cb2fa69d8e4363b7791215db3
AbdullahiAbdulganiyu <ludba020@gmail.com>
2025-04-29 14:30:04 +0100

    adjusting the gap

--- src/components/SectionWrapper.jsx
+++ src/components/SectionWrapper.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function SectionWrapper({ children, header, title }) {
   return (
     <section className="min-h-screen flex flex-col gap-10">
-      <div className="bg-slate-950 py-10 flex flex-col gap-4 justify-center items-center">
+      <div className="bg-slate-950 py-10 flex flex-col gap-2 justify-center items-center">
         <p>{header}</p>
         <h2>
           {title[0]}
