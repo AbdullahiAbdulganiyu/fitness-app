85ca845c77ea5e53333480e2ec9d4c23d88a9e93
AbdullahiAbdulganiyu <ludba020@gmail.com>
2025-04-30 23:07:41 +0100

    wrapping the children prop in a div and adding some classes to style the div

--- src/components/SectionWrapper.jsx
+++ src/components/SectionWrapper.jsx
@@ -11,7 +11,10 @@ export default function SectionWrapper({ children, header, title }) {
           {title[2]}
         </h2>
       </div>
-      {children}
+
+      <div className="max-w-[800px] w-full flex flex-col mx-auto gap-10">
+        {children}
+      </div>
     </section>
   );
 }
