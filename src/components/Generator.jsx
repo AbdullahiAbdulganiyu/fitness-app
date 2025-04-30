29d4e0553b515a69c39b63264a9244c7520713fc
AbdullahiAbdulganiyu <ludba020@gmail.com>
2025-04-30 22:43:48 +0100

    creating an header function

--- src/components/Generator.jsx
+++ src/components/Generator.jsx
@@ -1,6 +1,18 @@
 import React from "react";
 import SectionWrapper from "./SectionWrapper";
 
+function Header({ index, title, description }) {
+  return (
+    <div>
+      <div>
+        <p>{index}</p>
+        <h4>{title}</h4>
+      </div>
+      <p>{description}</p>
+    </div>
+  );
+}
+
 export default function Generator() {
   return (
     <div>
