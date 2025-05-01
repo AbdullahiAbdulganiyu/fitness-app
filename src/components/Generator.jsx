b129ccd705d4fead1fe7623d474fd8d447c246a1
AbdullahiAbdulganiyu <ludba020@gmail.com>
2025-05-01 14:37:15 +0100

    using the showmodal to conditionally render a div

--- src/components/Generator.jsx
+++ src/components/Generator.jsx
@@ -56,6 +56,7 @@ export default function Generator() {
             <p>Select muscle groups</p>
             <i className="fa-solid fa-caret-down absolute right-3 top-1/2 -translate-y-1/2"></i>
           </button>
+          {showModal && <div>Modal</div>}
         </div>
       </SectionWrapper>
     </div>
