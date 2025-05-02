8f31a2b9aed032f64667bfee1bc392408b68c7d7
AbdullahiAbdulganiyu <ludba020@gmail.com>
2025-05-02 21:40:40 +0100

    creating the button component

---
+++ src/components/Button.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Button({ text }) {
+  return (
+    <div>
+      <button className="px-8 py-4 rounded-md border-[2px] bg-slate-950 border-blue-400 border-solid blueShadow duration-200">
+        <p>{text}</p>
+      </button>
+    </div>
+  );
+}
