1536a4160c229947c7bbc8f9a449ed83480ea450
AbdullahiAbdulganiyu <ludba020@gmail.com>
2025-05-02 21:56:01 +0100

    adding some styling

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Button({ children }) {
   return (
     <div>
-      <button className="px-8 py-4 rounded-md border-[2px] bg-slate-950 border-blue-400 border-solid blueShadow duration-200">
+      <button className="px-8 mx-auto py-4 rounded-md border-[2px] bg-slate-950 border-blue-400 border-solid blueShadow duration-200">
         <p>{children}</p>
       </button>
     </div>
