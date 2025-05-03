51f755d52742dd3a54552689398a07f5e6d1b735
AbdullahiAbdulganiyu <ludba020@gmail.com>
2025-05-03 14:43:48 +0100

    creating a workout state

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import Generator from "./components/Generator";
 import Workout from "./components/Workout";
 import Hero from "./components/Hero";
 
 function App() {
+  const [workout, setWorkout] = useState(null);
   return (
     <main className="min-h-screen flex flex-col bg-gradient-to-r from-slate-800 to-slate-950 text-white text-sm sm:text-base">
       <Hero />
