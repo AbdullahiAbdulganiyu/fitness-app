c88100b29ff82211e5ef9221177bbd22ed1ba26c
AbdullahiAbdulganiyu <ludba020@gmail.com>
2025-05-04 14:21:11 +0100

    lifting up some state

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,9 @@ import Hero from "./components/Hero";
 
 function App() {
   const [workout, setWorkout] = useState(null);
+  const [poison, setPoison] = useState("individual");
+  const [muscles, setMuscles] = useState([]);
+  const [goal, setGoal] = useState("strength_power");
   return (
     <main className="min-h-screen flex flex-col bg-gradient-to-r from-slate-800 to-slate-950 text-white text-sm sm:text-base">
       <Hero />
