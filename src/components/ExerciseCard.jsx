6dc279b7fa1cbcec9c584d3dfe37486e7cb50b36
AbdullahiAbdulganiyu <ludba020@gmail.com>
2025-05-04 18:07:54 +0100

    adding some h2 and some classes to style them

--- src/components/ExerciseCard.jsx
+++ src/components/ExerciseCard.jsx
@@ -3,7 +3,11 @@ import React from "react";
 export default function ExerciseCard({ excercise, index }) {
   return (
     <div className="p-4 rounded-md flex flex-col gap-4 bg-slate-950 sm:flex-wrap">
-      <div className="flex flex-col sm:flex-row sm:items-center sm:flex-wrap gap-x-4 "></div>
+      <div className="flex flex-col sm:flex-row sm:items-center sm:flex-wrap gap-x-4 ">
+        <h2 className="text-3xl hidden sm:inline sm:text-4xl md:text-5xl font-semibold text-slate-400">
+          0{index + 1}
+        </h2>
+      </div>
     </div>
   );
 }
