6d4149c087fedd9b8c1750a58d039a75d0197937
AbdullahiAbdulganiyu <ludba020@gmail.com>
2025-05-04 17:54:43 +0100

    mapping over the workout object

--- src/components/Workout.jsx
+++ src/components/Workout.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import SectionWrapper from "./SectionWrapper";
+import ExerciseCard from "./ExerciseCard";
 
 export default function Workout({ Workout }) {
   return (
-    <SectionWrapper
-      header={"Welcome to "}
-      title={["The ", "DANGER", " zone"]}
-    ></SectionWrapper>
+    <SectionWrapper header={"Welcome to "} title={["The ", "DANGER", " zone"]}>
+      <div className="flex flex-col gap-4 ">
+        {Workout.map((exercise, index) => (
+          <ExerciseCard exercise={exercise} key={index} />
+        ))}
+      </div>
+    </SectionWrapper>
   );
 }
