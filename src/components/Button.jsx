ec88d82fccbb47c7a3a82c2dbf4bf0a86ddfd665
AbdullahiAbdulganiyu <ludba020@gmail.com>
2025-05-04 14:55:34 +0100

    using the function to handle click on the button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,7 +3,10 @@ import React from "react";
 export default function Button({ children, funct }) {
   return (
     <div>
-      <button className="px-8 mx-auto py-4 rounded-md border-[2px] bg-slate-950 border-blue-400 border-solid blueShadow duration-200">
+      <button
+        onClick={funct}
+        className="px-8 mx-auto py-4 rounded-md border-[2px] bg-slate-950 border-blue-400 border-solid blueShadow duration-200"
+      >
         <p>{children}</p>
       </button>
     </div>
