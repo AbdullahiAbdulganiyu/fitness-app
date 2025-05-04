1f681819a8a23089d6f1e67a19164c86956f7656
AbdullahiAbdulganiyu <ludba020@gmail.com>
2025-05-04 18:05:03 +0100

    adding some div and adding some classes to style them

--- src/components/ExerciseCard.jsx
+++ src/components/ExerciseCard.jsx
@@ -1,5 +1,9 @@
 import React from "react";
 
-export default function ExerciseCard() {
-  return <div>Exercise Card</div>;
+export default function ExerciseCard({ excercise, index }) {
+  return (
+    <div className="p-4 rounded-md flex flex-col gap-4 bg-slate-950 sm:flex-wrap">
+      <div className="flex flex-col sm:flex-row sm:items-center sm:flex-wrap gap-x-4 "></div>
+    </div>
+  );
 }
