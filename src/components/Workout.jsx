55c7513e85a2007b344b5aebbf6a5f9c5c086ad0
AbdullahiAbdulganiyu <ludba020@gmail.com>
2025-05-05 09:35:29 +0100

    fixing some naming mistake

--- src/components/Workout.jsx
+++ src/components/Workout.jsx
@@ -2,11 +2,11 @@ import React from "react";
 import SectionWrapper from "./SectionWrapper";
 import ExerciseCard from "./ExerciseCard";
 
-export default function Workout({ Workout }) {
+export default function Workout({ workout }) {
   return (
     <SectionWrapper header={"Welcome to "} title={["The ", "DANGER", " zone"]}>
       <div className="flex flex-col gap-4 ">
-        {Workout.map((exercise, index) => (
+        {workout.map((exercise, index) => (
           <ExerciseCard exercise={exercise} index={index} key={index} />
         ))}
       </div>
