9a2b54813d2e60a0da9bffabe76555123487e149
AbdullahiAbdulganiyu <ludba020@gmail.com>
2025-05-05 16:03:08 +0100

    adding href

--- src/App.jsx
+++ src/App.jsx
@@ -17,6 +17,7 @@ function App() {
     let newWorkout = generateWorkout({ poison, goal, muscles });
     console.log(newWorkout);
     setWorkout(newWorkout);
+    window.location.href = "#workout";
   }
   return (
     <main className="min-h-screen flex flex-col bg-gradient-to-r from-slate-800 to-slate-950 text-white text-sm sm:text-base">
